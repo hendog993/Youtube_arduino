ea9fc6a65d9d5f8acee08528c1fb3db349b48b01
Henry V. Gilbert <henrygilbert609@gmail.com>
2021-09-27 17:44:04 -0500

    Add files via upload

--- Flight_Simulator.ino
+++ Flight_Simulator.ino
@@ -2,8 +2,9 @@
 /*      Interface Control
  * ========= Analog Pins =========         
  * pin A0 - Potentionmeter Input       
- *       
- *       
+ * pin A1 - Joystick Input VRx       
+ * pin A2 - Joystick Input VRy       
+ *        
  * ========= Digital Pins =========      
  * pin  2 - Rotary Encoder DT      !! Interrupt Driven Pin
  * pin  3 - Rotary Encoder CLK     !! Interrupt Driven Pin
@@ -25,10 +26,8 @@
 
 
 // =================== PIN DECLARATIONS ====================
-/*
- * LCD Pins
- */
 
+// LCD Pins 
 #define lcd_rs 22
 #define lcd_e  23
 #define lcd_d4 24
@@ -36,19 +35,60 @@
 #define lcd_d6 26
 #define lcd_d7 27
 
-LiquidCrystal lcd ( lcd_rs, lcd_e, lcd_d4, lcd_d5, lcd_d6,lcd_d7);
+// Pot Input pin
+#define throttle_in A0
+
+// Rotary Encoder Pins
+#define rotEnc_dt 2
+#define rotEnc_clk 3
+
+// Joy Stick Pins
+#define js_vrx A1
+#define js_vry A2
+#define js_sw  4
+
+// Digital Output
+#define status_light 12
+
+
+// Object Declarations 
+LiquidCrystal lcd ( lcd_rs, lcd_e, lcd_d4, lcd_d5, lcd_d6, lcd_d7 );
 
 void setup() {
   // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
+   Serial.begin( 9600 );
+  lcd.begin( 16, 2 );
+  pinMode( js_vrx, INPUT );
+  pinMode( js_vry, INPUT );
+  pinMode( js_sw , INPUT );
+  pinMode( rotEnc_dt, INPUT );
+  pinMode( rotEnc_clk, INPUT );
+ 
 }
 
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
+ 
+  lcd.setCursor( 0, 1 );
   // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+  lcd.print( millis() / 1000 );
+
+  input_test();
+}
+
+
+int32_t input_test () {
+    // Create local variables and test validity of input data. 
+
+    
+    Serial.print ( "Analog Input Pot : " ) ;
+    Serial.print ( analogRead( throttle_in ) ) ;
+    Serial.print( "    Joystick X :" ) ;
+    Serial.print ( analogRead ( js_vrx ) ) ;
+ 
+    Serial.print( "    Joystick Y :" ) ;
+    Serial.print ( analogRead ( js_vry ) ) ;
+    Serial.print( "    Joystick SW :" ) ;
+    Serial.print ( digitalRead ( js_sw ) ) ;
+        Serial.print ( "\n " );
+    return 0;
 }
