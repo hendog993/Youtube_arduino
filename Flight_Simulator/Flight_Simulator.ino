832310fc31ab867c6fcbb571364bb8cadd386be4
Henry Gilbert <henrygilbert609@gmail.com>
2021-10-11 17:20:39 -0500

    Revert "Update Flight_Simulator.ino"
    This reverts commit b0e429cc64e97aefa1fc096b3433e767b1662406.

--- Flight_Simulator/Flight_Simulator.ino
+++ Flight_Simulator/Flight_Simulator.ino
@@ -296,11 +296,7 @@ int32_t thaw_true_airspeed (Frozen_Data * frozenPtr,  Thawed_Data * thawedPtr )
     float A = 0.00073f;
     float D = .25f;
     
-<<<<<<< HEAD
     new_true_airspeed= old_airspeed + (A * frozenPtr->analog_pot - D);  
-=======
-    thawedPtr->true_airspeed = old_airspeed + (A * frozenPtr->analog_pot - D);  
->>>>>>> parent of f90869b (Basic changes)
 
     // Limit the airspeed between the min 0 and the max 176. 
     if ( new_true_airspeed > MAX_AIRSPEED ) new_true_airspeed  = MAX_AIRSPEED ; 
@@ -313,7 +309,6 @@ int32_t thaw_true_airspeed (Frozen_Data * frozenPtr,  Thawed_Data * thawedPtr )
     return s32_returnval ;
 }
 
-<<<<<<< HEAD
 
 
 
@@ -326,8 +321,6 @@ int32_t thaw_true_airspeed (Frozen_Data * frozenPtr,  Thawed_Data * thawedPtr )
    Description -            
 */
 
-=======
->>>>>>> parent of f90869b (Basic changes)
 int32_t thaw_groundspeed ( Frozen_Data * frozenPtr,  Thawed_Data * thawedPtr ) 
 
 {
